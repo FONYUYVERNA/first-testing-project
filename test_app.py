f881b10ad21bb6d91f85887fb258738c16a6d4fd
FONYUYVERNA <verenamfonyuy@gmail.com>
2024-11-06 16:04:39 +0100

    added some files

--- test_app.py
+++ test_app.py
@@ -1,14 +1,14 @@
 import unittest
-from app import app
+from app import app #This line imports the flask application intance from app module
 import json
 
 class TestFlaskApp(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): #this is the start of setup method which is a special method in unit test that is run before each test method in the class
         # Create a test client
-        self.app = app.test_client()
+        self.app = app.test_client() 
         self.app.testing = True
 
-    def test_home_route(self):
+    def test_home_route(self): #this line defines a test method that starts with the word test
         # Send a GET request to the home route
         response = self.app.get('/')
         
